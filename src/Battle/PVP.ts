aada2ed8d9e686cde6a5c22cf0d5e6226e25c011
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-10 14:56:39 -0300

    feat: Create PVP class.
    It is a extension of Battle. It receive two figters evaluate the battle to define who will survive.

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,37 @@
+// import Character from '../Character';
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private _player2: Fighter;
+
+  constructor(player: Fighter, player2: Fighter) {
+    super(player);
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    // let counter = 0;
+    console.log('ANTES');
+    console.log(this.player.lifePoints);
+    console.log(this._player2.lifePoints);
+    while (this.player.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this.player.attack(this._player2);
+      this._player2.attack(this.player);
+      console.log('------------');
+      console.log(this.player.lifePoints);
+      console.log(this._player2.lifePoints);
+      console.log('------------');
+      // counter += 1;
+      // if (counter > 10) {
+      //   console.log(this.player.lifePoints);
+      //   console.log(this._player2.lifePoints);
+      //   break;
+      // }
+    }
+    console.log('Depois');
+    console.log(this.player.lifePoints);
+    console.log(this._player2.lifePoints);
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
