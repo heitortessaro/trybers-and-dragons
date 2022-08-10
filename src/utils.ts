88420a94d3eba742cf8aef711fd6b56540a662f8
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-10 16:06:55 -0300

    feat:

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,17 @@
+import { SimpleFighter } from './Fighter';
+
 function getRandomInt(min: number, max: number) {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
 }
 
+function getRandomEnemy(array:SimpleFighter[]): number {
+  const numberOfEnemies:number = array.length;
+  const enemyNumber = Math.floor(Math.random() * numberOfEnemies);
+  return enemyNumber;
+}
+
 export default getRandomInt;
+
+export { getRandomEnemy };
