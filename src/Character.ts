f3d823f2698eba0f2daaedca171f75dcc4dcdaf3
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-09 22:08:25 -0300

    feat: Create class Monster.
    It implements the SimpleFighter interface. To make the system works, Charachter need to use SimpleFighter as type for attack and special methods.

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -77,7 +77,7 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  public attack(enemy: Fighter): void {
+  public attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
@@ -93,7 +93,7 @@ export default class Character implements Fighter {
     this._lifePoints = this._maxLifePoints;
   }
 
-  public special(enemy: Fighter): void {
+  public special(enemy: SimpleFighter): void {
     enemy.receiveDamage(
       (this._strength + this._dexterity) * getRandomInt(1, 4),
     );
